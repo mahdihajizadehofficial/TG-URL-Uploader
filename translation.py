b7bfb39a5707f234b15b7c187cf7f15301f54b38
mahdihajizadehofficial <75172352+mahdihajizadehofficial@users.noreply.github.com>
2020-11-28 10:44:39 -0800

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,5 +1,5 @@
 class Translation(object):
-    START_TEXT = """   Hello   """
+    START_TEXT = """   Hello       HHHHHHHHHHHHHHhh   """
     RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
     ABS_TEXT = " Please don't be selfish."
     UPGRADE_TEXT = "<b>👉 Create own Clone Bot.. </b>  /help for Details"
